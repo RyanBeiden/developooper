b376597861a76b6d162f7f3facfefa7e18141088
Ryan Beiden <ryanbeiden@pm.me>
2026-06-21 00:31:26 -0500

    Include nav items and icons for new packages

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -4,7 +4,7 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
-        <flux:sidebar sticky collapsible="mobile" class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+        <flux:sidebar sticky collapsible class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.header>
                 <x-app-logo :sidebar="true" href="{{ route('dashboard') }}" wire:navigate />
                 <flux:sidebar.collapse class="lg:hidden" />
@@ -15,18 +15,29 @@
                     <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </flux:sidebar.item>
+                    <flux:sidebar.group expandable expanded="false" heading="Monitoring" class="grid">
+                        <flux:sidebar.item icon="gauge" :href="route('horizon.index')" target="_blank">
+                            {{ __('Horizon') }}
+                        </flux:sidebar.item>
+                        <flux:sidebar.item icon="activity" :href="route('pulse')" target="_blank">
+                            {{ __('Pulse') }}
+                        </flux:sidebar.item>
+                        <flux:sidebar.item icon="telescope" :href="route('telescope')" target="_blank">
+                            {{ __('Telescope') }}
+                        </flux:sidebar.item>
+                    </flux:sidebar.group>
                 </flux:sidebar.group>
             </flux:sidebar.nav>
 
             <flux:spacer />
 
             <flux:sidebar.nav>
-                <flux:sidebar.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
+                <flux:sidebar.item icon="folder-git-2" href="https://github.com/RyanBeiden/developooper" target="_blank">
                     {{ __('Repository') }}
                 </flux:sidebar.item>
 
-                <flux:sidebar.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                    {{ __('Documentation') }}
+                <flux:sidebar.item icon="toilet" href="https://github.com/users/RyanBeiden/projects/1" target="_blank">
+                    {{ __('The Toilet') }}
                 </flux:sidebar.item>
             </flux:sidebar.nav>
 
